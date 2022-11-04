e2b771471b00e58e18ffa1fa3529c543d5e96100
unknown <chandru151998@gmail.com>
2022-11-04 15:02:07 +0530

    [Chandru]store messages in database

--- chat/models.py
+++ chat/models.py
@@ -6,8 +6,12 @@ class Group(models.Model):
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
     members = models.ManyToManyField('user.User', related_name='members')
 
-#model - messages
-#group - frn key grup model
-#user - frn key user model
-#message - txtField()
-#date time createdat
+
+class Messages(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    message = models.TextField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
