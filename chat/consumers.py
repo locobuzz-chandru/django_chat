1896d08698ecec3b85d254c1eb6ffcc36fca5b32
unknown <chandru151998@gmail.com>
2023-03-03 00:10:23 +0530

    [Chandru]Added. refactored consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -21,11 +21,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        print(message)
         username = text_data_json['username']
-        print(username)
         room_name = text_data_json['room_name']
-        print(room_name)
 
         # Send message to room group
         await self.save_messages(room_name, username, message)
@@ -43,9 +40,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def save_messages(self, room_name, username, message):
-        group = Group.objects.get(name=room_name, members__username=username)
-        print(group.id)
-        print(group.user)
-        print(username)
-        print(message)
-        Messages.objects.create(group=group, user=username, message=message)
+        if message == "":
+            pass
+        else:
+            group = Group.objects.get(name=room_name, members__username=username)
+            Messages.objects.create(group=group, user=username, message=message)
